2c2593619b626bb2474d22f88235498f4d8bbe56
Dillon Beliveau <dillonbeliveau@gmail.com>
2018-02-04 15:38:05 -0500

    Cache results for 10 seconds

--- src/main/java/com/dillonbeliveau/ts3status/service/TS3Service.java
+++ src/main/java/com/dillonbeliveau/ts3status/service/TS3Service.java
@@ -3,10 +3,14 @@ package com.dillonbeliveau.ts3status.service;
 import com.dillonbeliveau.ts3status.model.ParsedClient;
 import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Component
@@ -14,8 +18,32 @@ public class TS3Service {
     @Autowired
     private TS3Api api;
 
+    private LoadingCache<CacheKey, List<Client>> clients;
+
+    private enum CacheKey {
+        Online
+    }
+
+    public TS3Service() {
+        clients = CacheBuilder.newBuilder()
+                .expireAfterWrite(10, TimeUnit.SECONDS)
+                .build(new CacheLoader<CacheKey, List<Client>>() {
+                    @Override
+                    public List<Client> load(CacheKey cacheKey) {
+                        switch (cacheKey) {
+                            case Online:
+                                return api.getClients();
+                            default:
+                                throw new IllegalArgumentException("Illegal cache key: " + cacheKey);
+
+                        }
+                    }
+                });
+    }
+
+
     public List<ParsedClient> getAllClients() {
-        return api.getClients().stream()
+        return clients.getUnchecked(CacheKey.Online).stream()
                 .filter(Client::isRegularClient)
                 .map(ParsedClient::new)
                 .collect(Collectors.toList());
