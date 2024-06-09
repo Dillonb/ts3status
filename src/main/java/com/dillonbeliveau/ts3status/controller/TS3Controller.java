bafe9d51f29150aaebbe0ca8e09837493f9bcae8
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-06-09 15:14:10 -0700

    generate html clientside, auto refresh

--- src/main/java/com/dillonbeliveau/ts3status/controller/TS3Controller.java
+++ src/main/java/com/dillonbeliveau/ts3status/controller/TS3Controller.java
@@ -1,20 +1,13 @@
 package com.dillonbeliveau.ts3status.controller;
 
-import com.dillonbeliveau.ts3status.repository.ParsedClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TS3Controller {
-    @Autowired
-    private ParsedClientRepository parsedClientRepository;
-
     @RequestMapping("/")
     public String clients(Model model) {
-        model.addAttribute("clients", parsedClientRepository.findByOnlineTrue());
-        model.addAttribute("offlineClients", parsedClientRepository.findByOnlineFalse());
         return "clients";
     }
 }
